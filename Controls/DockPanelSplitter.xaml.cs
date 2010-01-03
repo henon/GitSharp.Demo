1d1523ce12cae0da3ebcb2f4674499a7ec3abb9b
henon <meinrad.recheis@gmail.com>
2010-01-04 00:35:28 +0100

    included information about original author and copyright in DockPanelSplitter

--- Controls/DockPanelSplitter.xaml.cs
+++ Controls/DockPanelSplitter.xaml.cs
@@ -1,9 +1,88 @@
-﻿using System.Diagnostics;
+﻿/*
+ * Taken from http://www.codeproject.com/KB/WPF/DockPanelSplitter.aspx and changed slightly by henon.
+ * Original License: http://www.codeproject.com/info/cpol10.aspx
+ * 
+ * Copyright (C) 2009, objo
+ * Copyright (C) 2010, Henon <meinrad.recheis@gmail.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the project nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/// Code Project article
+/// http://www.codeproject.com/KB/WPF/DockPanelSplitter.aspx
+/// 
+/// CodePlex project
+/// http://wpfcontrols.codeplex.com
+///
+/// DockPanelSplitter is a splitter control for DockPanels.
+/// Add the DockPanelSplitter after the element you want to resize.
+/// Set the DockPanel.Dock to define which edge the splitter should work on.
+///
+/// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+///
+/// Step 1a) Using this custom control in a XAML file that exists in the current project.
+/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+/// to be used:
+///
+///     xmlns:OsDps="clr-namespace:DockPanelSplitter"
+///
+///
+/// Step 1b) Using this custom control in a XAML file that exists in a different project.
+/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+/// to be used:
+///
+///     xmlns:MyNamespace="clr-namespace:DockPanelSplitter;assembly=DockPanelSplitter"
+///
+/// You will also need to add a project reference from the project where the XAML file lives
+/// to this project and Rebuild to avoid compilation errors:
+///
+///     Right click on the target project in the Solution Explorer and
+///     "Add Reference"->"Projects"->[Select this project]
+///
+///
+/// Step 2)
+/// Go ahead and use your control in the XAML file.
+///
+///     <MyNamespace:DockPanelSplitter/>
+///
+
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Chili.WPF.Controls
+namespace GitSharp.Demo.Controls
 {
     /// <summary>
     /// DockPanelSplitter is a splitter control for DockPanels.
