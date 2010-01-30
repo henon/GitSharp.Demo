9c96b71563e624572c87763f7cf10cc390f03953
henon <meinrad.recheis@gmail.com>
2010-01-31 00:48:42 +0100

    commit diff view now notifies via event if selection has changed.

--- CommitDiffView.xaml.cs
+++ CommitDiffView.xaml.cs
@@ -36,68 +36,89 @@
  */
 
 using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
 
 namespace GitSharp.Demo
 {
-    /// <summary>
-    /// Interaction logic for CommitDiffView.xaml
-    /// </summary>
-    public partial class CommitDiffView 
-    {
-        public CommitDiffView()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for CommitDiffView.xaml
+	/// </summary>
+	public partial class CommitDiffView
+	{
+		public CommitDiffView()
+		{
+			InitializeComponent();
+			m_treediff.SelectionChanged += OnSelectionChanged;
+		}
 
-        public void Init(Commit c1, Commit c2)
-        {
-            if (c1==null)
-            {
-                m_treediff.ItemsSource = null;
-                return;
-            }
-            //m_title.Content = "Differences between commits " + c1.ShortHash + " and " + c2.ShortHash;
-            var changes=c1.CompareAgainst(c2);
-            m_treediff.ItemsSource = changes;
-        }
+		public event Action<Change> SelectionChanged;
 
-        //private void onClose(object sender, RoutedEventArgs e)
-        //{
-        //    //Closing application
-        //    this.Close();
-        //}
-    }
+		private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			FireSelectionChanged();
+		}
 
-    public class ChangeColorConverter : IValueConverter
-    {
-        #region IValueConverter Members
+		private void FireSelectionChanged()
+		{
+			if (SelectionChanged != null)
+				SelectionChanged(m_treediff.SelectedValue as Change);
+		}
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            var change = value as Change;
-            if (change == null)
-                return Brushes.Black;
-            switch (change.ChangeType)
-            {
-                case ChangeType.Added:
-                    return Brushes.Plum;
-                case ChangeType.Deleted:
-                    return Brushes.Red;
-                case ChangeType.Modified:
-                    return Brushes.RoyalBlue;
-                default:
-                    return Brushes.Black;
-            }
-        }
+		public void Init(Commit c1, Commit c2)
+		{
+			if (c1 == null)
+			{
+				m_treediff.ItemsSource = null;
+				return;
+			}
+			//m_title.Content = "Differences between commits " + c1.ShortHash + " and " + c2.ShortHash;
+			var changes = c1.CompareAgainst(c2);
+			m_treediff.ItemsSource = changes;
+			if (changes.Count() > 0)
+			{
+				m_treediff.SelectedIndex = 0;
+			}
+			FireSelectionChanged();
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+		//private void onClose(object sender, RoutedEventArgs e)
+		//{
+		//    //Closing application
+		//    this.Close();
+		//}
+	}
 
-        #endregion
-    }
+	public class ChangeColorConverter : IValueConverter
+	{
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var change = value as Change;
+			if (change == null)
+				return Brushes.Black;
+			switch (change.ChangeType)
+			{
+				case ChangeType.Added:
+					return Brushes.Plum;
+				case ChangeType.Deleted:
+					return Brushes.Red;
+				case ChangeType.Modified:
+					return Brushes.RoyalBlue;
+				default:
+					return Brushes.Black;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
 }
